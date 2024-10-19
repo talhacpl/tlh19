633bd513dcedac43882aaf86af06381c87197801
Talha Çöplü <talhacoplu@gmail.com>
2024-10-19 23:15:57 +0300

    V3

---
+++ src/ders01/Ders03.java
@@ -0,0 +1,16 @@
+package ders01;
+
+public class Ders03 {
+
+    public static void main(String[] args) {
+
+        System.out.println("FENERBAHÇE");
+        System.out.println("FENERBAHÇE");
+        System.out.println("FENERBAHÇE");
+        System.out.println("FENERBAHÇE");
+        System.out.println("FENERBAHÇE");
+        System.out.println("FENERBAHÇE");
+        System.out.println("FENERBAHÇE");
+        System.out.println("FENERBAHÇE");
+    }
+}
