16e9e5983df1188494840d2aca87d01a0cf5f6d5
talhacpl <114307516+talhacpl@users.noreply.github.com>
2024-10-19 23:09:26 +0300

    Update Ders02.java

--- src/ders01/Ders02.java
+++ src/ders01/Ders02.java
@@ -4,5 +4,11 @@ public class Ders02 {
 
     public static void main(String[] args) {
         System.out.println("bbbbbvvvvvvccccccc");
+        System.out.println("bbbbbvvvvvvccccccc");
+        System.out.println("bbbbbvvvvvvccccccc");
+        System.out.println("bbbbbvvvvvvccccccc");
+        System.out.println("bbbbbvvvvvvccccccc");
+        System.out.println("bbbbbvvvvvvccccccc");
+        System.out.println("bbbbbvvvvvvccccccc");
     }
 }
