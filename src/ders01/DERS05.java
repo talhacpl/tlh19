c78aa426afddb681143516392b76ab8807f68299
Talha Çöplü <talhacoplu@gmail.com>
2024-10-27 22:44:56 +0300

    CV

---
+++ src/ders01/DERS05.java
@@ -0,0 +1,26 @@
+package ders01;
+
+public class DERS05 {
+
+    public static void main(String[] args) {
+
+        int sonuc =topla(23,12,15);
+        System.out.println(sonuc
+        );
+
+
+
+    }
+
+    public static int topla (int a,int b) {
+
+        return a+b;
+
+    }
+
+    public static double topla (double a,double b) {
+
+        return a+b;
+
+    }
+}
